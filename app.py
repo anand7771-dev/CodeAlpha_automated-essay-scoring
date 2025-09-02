aed1a7b51a21c3cbf6719890e731a1fb1f01fead
anand7771-dev <ananddev4423169@gmail.com>
2025-09-02 15:40:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 
-# Load your saved model and vectorizer
+# Load model and vectorizer
 model = pickle.load(open("models/best_model.pkl", "rb"))
 tfidf = pickle.load(open("models/tfidf_vectorizer.pkl", "rb"))
 le = pickle.load(open("models/label_encoder.pkl", "rb"))
@@ -11,15 +11,7 @@ st.title("Automated Essay Scoring")
 input_text = st.text_area("Enter your essay here")
 
 if st.button("Predict Score"):
-    # Transform the input text using TF-IDF
     features = tfidf.transform([input_text])
-    
-    # Predict using your model
     prediction = model.predict(features)
-    
-    # Decode label if using label encoder
-    if le:
-        prediction = le.inverse_transform(prediction)
-    
+    prediction = le.inverse_transform(prediction)
     st.write("Predicted Score:", prediction[0])
-
